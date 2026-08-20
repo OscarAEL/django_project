a25025558f375017d385b8d4981a659d836d4098
OscarAEL <oscar.eneque.l@tecsup.edu.pe>
2026-08-19 19:53:28 -0500

    Corregir indentacion del modelo Item

--- src/core/models.py
+++ src/core/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Item(models.Model):
-	name = models.CharField(max_length=200)
-	description = models.TextField(blank=True)
-	created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return str(self.name)
